b119e969e3373ad2aacfc61131f5b0c3363a15d4
samkc-0 <sam.k.c.0@icloud.com>
2025-10-22 22:34:57 +0100

    connect to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ")
+
 	fmt.Println("Starting Peril server...")
+
+	// Ctrl (or Cmd) + C to exit
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	fmt.Println("Press ^C to exit")
+	<-sigs
+	fmt.Println("Shutting down Peril server...")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module peril
 
 go 1.25.1
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
